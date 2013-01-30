e3491eb95276bd1dc82dfcc8bc3d9faf9e06fa3c
gigo101 <ianjay.padios@gmail.com>
2013-01-30 18:41:27 +0800

    add sample collections

---
+++ SampleCollections/src/samplecollections/SampleCollections.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samplecollections;
+
+/**
+ *
+ * @author ian val
+ */
+public class SampleCollections {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
