40b6d5838498690295d801fca4f0f8c875922fad
gigo101 <ianjay.padios@gmail.com>
2013-01-30 18:39:23 +0800

    add project123

---
+++ project123/src/javaconnect.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ppp
+ */
+import java.sql.*;
+import javax.swing.*;
+
+public class javaconnect {
+    
+    static Connection conn=null;
+    public static Connection ConnecrDb(){
+    
+        try{
+        
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/project123","root","");
+            JOptionPane.showMessageDialog(null, "Connection established");
+            return conn;
+        } catch(Exception e){
+            
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        
+        }
+    }
+    
+}
